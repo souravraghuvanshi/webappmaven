32319f61e59deaf038c849f0a50ee523ae8b6ddf
souravraghuvanshi <47347584+souravraghuvanshi@users.noreply.github.com>
2019-05-10 13:39:41 +0530

    Update sourav.tf

--- sourav.tf
+++ sourav.tf
@@ -1,11 +1,40 @@
 
-provider "aws" {
-  region     = "us-east-1"
+resource "aws_cloudwatch_log_group" "souravraghuvanshi/sourav-jenkins-webapp:ver1" {
+  name              = "souravraghuvanshi/sourav-jenkins-webapp:ver1"
+  retention_in_days = 1
 }
 
-resource "aws_instance" "example" {
-  ami           = "ami-0a313d6098716f372"
-  instance_type = "t2.micro"
-  key_name 	= "sourav-rag"
-} 
+resource "aws_ecs_task_definition" "souravraghuvanshi/sourav-jenkins-webapp:ver1" {
+  family = "souravraghuvanshi/sourav-jenkins-webapp:ver1"
+
+  container_definitions = <<EOF
+[
+  {
+    "name": "souravraghuvanshi/sourav-jenkins-webapp:ver1",
+    "image": "souravraghuvanshi/sourav-jenkins-webapp:ver1",
+    "cpu": 0,
+    "memory": 32,
+    "logConfiguration": {
+      "logDriver": "awslogs",
+      "options": {
+        "awslogs-region": "us-east-1",
+        "awslogs-group": "souravraghuvanshi/sourav-jenkins-webapp:ver1",
+        "awslogs-stream-prefix": "complete-ecs"
+      }
+    }
+  }
+]
+EOF
+}
+
+resource "aws_ecs_service" "souravraghuvanshi/sourav-jenkins-webapp:ver1" {
+  name            = "souravraghuvanshi/sourav-jenkins-webapp:ver1d"
+  cluster         = "${var.cluster_id}"
+  task_definition = "${aws_ecs_task_definition.souravraghuvanshi/sourav-jenkins-webapp:ver1.arn}"
+
+  desired_count = 1
+
+  deployment_maximum_percent         = 100
+  deployment_minimum_healthy_percent = 0
+}
  
